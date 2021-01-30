83ce0d75435c1b73cb438639e8d05817e4e3b9dc
Sri <48895259+SriLikesToSing@users.noreply.github.com>
2021-01-30 13:53:10 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,12 +13,18 @@ memory buffer : turing number : tape description
 
 design of program:
   - divide the tape into 3 sections enstead of using 3 separate tapes (prob saves memory and increases efficiecy especially during 5 state busy beaver)
-
+        - wrong????? (1/30/2021)
   - Create a universal turing mahine class????
   - have a simulate function
 
     - the tape should be a vector<string>
         - give some dedicated amount of space to the buffer at the start
+            - the buffer will store the state it is currently in
+
+        - for the 3 tape design (vector<string> or a set):
+            - tape for the buffer
+            - tape for the machine description (array inside of array?)
+            - tape for the tape description (where m's computation is performed)
 
     - create a universalTuringMachine Class
       - given some input simulate and give an output
@@ -38,6 +44,8 @@ design of program:
                 - halt()
 
                 - nextState()
+
+                - should you also impliment the functions in the paper? *dunno
 
 
        - simulate() function outputs the resulting tape after computation (output)
