51ed50a21b461fac88917f4a4047c4638298468b
Sri <48895259+SriLikesToSing@users.noreply.github.com>
2021-02-03 20:44:58 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,24 +8,18 @@ using namespace std;
 
 /*
 memory buffer : turing number : tape description
-
-
-
 design of program:
   - divide the tape into 3 sections enstead of using 3 separate tapes (prob saves memory and increases efficiecy especially during 5 state busy beaver)
         - wrong????? (1/30/2021)
   - Create a universal turing mahine class????
   - have a simulate function
-
     - the tape should be a vector<string>
         - give some dedicated amount of space to the buffer at the start
             - the buffer will store the state it is currently in
-
         - for the 3 tape design (vector<string> or a set):
             - tape for the buffer
             - tape for the machine description (array inside of array?)
             - tape for the tape description (where m's computation is performed)
-
     - create a universalTuringMachine Class
       - given some input simulate and give an output
            - class variables (strings or long longs)
@@ -36,7 +30,6 @@ design of program:
                 - tuple of permissable symbols
                 - array inside of array of each rule
                 - hard code the three permissable actions {L, N, R}
-
             - init function
                 - the input tape
                 and class variables
@@ -47,11 +40,8 @@ design of program:
                     - change tape of memory buffer
                     - change contents of the tape
                 - should you also impliment the functions in the paper? *dunno
-
-
        - simulate() function outputs the resulting tape after computation (output)
             -
-
 */
 
 class universalTuringMachine{
@@ -65,7 +55,13 @@ class universalTuringMachine{
     string bufferTape[2];
     //a vector is theoretically infinite????
     //vector<string> tapeDescription = {"1", "1", "1"};
-    vector<string> tapeDescription = {};
+    //vector<string> tapeDescription = {};
+
+    long long startIndex = 5000;
+    long long endIndex = 10000-1;
+    string tapeDescription[10000];
+
+
 
     public:
         void setValues(vector<string>, string, string, string, vector<string>, vector<vector<string>>);
@@ -86,17 +82,25 @@ void universalTuringMachine::setValues (vector<string> States, string InitialSta
     blankSymbol = BlankSymbol;
     permissableSymbols = PermissableSymbols;
     machineDescriptionTape = Rules;
+    for(int x=0; x<10000; x++){
+        tapeDescription[x] = "0";
+        }
+        /*
+    tapeDescription[startIndex] = "1";
+    tapeDescription[startIndex+1] = "1";
+    tapeDescription[startIndex+2] = "1";
+    */
     }
 
 void universalTuringMachine::halt(){
-    for(int x=0; x<tapeDescription.size(); x++){
+    for(int x=0; x<endIndex; x++){
         cout << tapeDescription[x] << " ";
     }
     cout << endl;
 }
 
 void universalTuringMachine::printTape(){
-    for(int x=0; x<tapeDescription.size(); x++){
+    for(int x=0; x<endIndex; x++){
         cout << tapeDescription[x] << " ";
     }
     cout << endl;
@@ -104,36 +108,31 @@ void universalTuringMachine::printTape(){
 
 void universalTuringMachine::simulate(){
     bool HALT = false;
-    if(tapeDescription.size() == 0){
-        tapeDescription.push_back(blankSymbol);
-    }
-    long long index = 0;
+    long long index = startIndex;
     bufferTape[0] = initialState;
-    bufferTape[1] = "1";
     long long count = 0;
     while(HALT == false){
-        cout << count << endl;
-        printTape();
+        //printTape();
         for(int x=0; x<machineDescriptionTape.size(); x++){
+            if(index == startIndex+3){
+                cout << "JLKJLKAJDLK"<<endl;
+                cout << tapeDescription[index]<< " " << machineDescriptionTape[x][1] << endl;
+            }
             if(machineDescriptionTape[x][0] == bufferTape[0] && machineDescriptionTape[x][1] == tapeDescription[index]){
-                cout << "BRO!"<<endl;
                 if(machineDescriptionTape[x][4] == terminatingState){
-                    cout << "TAPE" <<endl;
+                    cout << "ITS ENDING BRO" <<endl;
                     cout << machineDescriptionTape[x][1] << endl;
                     tapeDescription[index] = machineDescriptionTape[x][2];
                     halt();
                     HALT = true;
                     break;
                 }
-                cout << machineDescriptionTape[x][0] << endl;
+                cout << machineDescriptionTape[x][3] << endl;
                 bufferTape[0] = machineDescriptionTape[x][4];
-                cout << "buffer tape"<<endl;
-                cout << bufferTape[0]<<endl;
                 if(machineDescriptionTape[x][3] == "right"){
                     cout << "ITS MOVING RIGHT BRO!"<<endl;
-                    cout << index << endl;
+                    cout << tapeDescription[index] << endl;
                     tapeDescription[index] = machineDescriptionTape[x][2];
-                    tapeDescription.push_back(blankSymbol);
                     index++;
                     break;
                 }else if(machineDescriptionTape[x][3] == "left"){
@@ -142,6 +141,7 @@ void universalTuringMachine::simulate(){
                     index--;
                     break;
                 }else if(machineDescriptionTape[x][3] == "stay"){
+                    cout << "lol"<<endl;
                     tapeDescription[index] = machineDescriptionTape[x][2];
                     break;
                 }
@@ -153,17 +153,11 @@ void universalTuringMachine::simulate(){
 /*
 kororo = crying
 maitei = greeting
-
 karu = eating
-
 mo ai = wll not
 ma = were not, was
-
-
 akaruma = I was not eating
 ojupita =
-
-
 */
 
 
@@ -173,9 +167,9 @@ int main() {
     /*
     universalTuringMachine test;
     vector<string> state = {"q0", "qf"};
-    vector<string> symbols = {"B", "1"};
-    vector<vector<string>> rules = {{"q0", "1", "1", "right", "q0"}, {"q0", "B", "1", "stay", "qf"}};
-    test.setValues(state, "q0", "qf", "B", symbols, rules);
+    vector<string> symbols = {"0", "1"};
+    vector<vector<string>> rules = {{"q0", "1", "1", "right", "q0"}, {"q0", "0", "1", "stay", "qf"}};
+    test.setValues(state, "q0", "qf", "0", symbols, rules);
     test.simulate();
     */
 
@@ -188,5 +182,7 @@ int main() {
     threeStateBusyBeaver.setValues(state, "a", "halt", "0", symbols, rules);
     threeStateBusyBeaver.simulate();
 
+
+    // 5-state, 2-symbol probable Busy Beaver machine from wikipedia
 
 }
